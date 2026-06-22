9ff69b78bb0effd0c47a1e3b450d0382d56a3a6a
AdairAG <javier.adair.ag@gmail.com>
2026-06-22 12:52:25 -0600

    Se actualiza agendaLlena

--- src/Agenda.java
+++ src/Agenda.java
@@ -2,6 +2,11 @@ public class Agenda {
     private Contacto[] contactos;
     private int totalContactos;
 
+    public Agenda() {
+        this.contactos = new Contacto[10];
+        this.totalContactos = 0;
+    }
+
     public boolean agendaLlena() {
         return totalContactos == contactos.length;
     }
--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        Agenda agenda = new Agenda();
+        System.out.println(agenda.agendaLlena());
     }
-
-
 }
